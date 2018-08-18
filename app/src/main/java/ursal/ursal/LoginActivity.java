de860d0c8c99f5bb42a41fc41e8ed5cdf924f567
Bruno Weber <bruno998>
2018-08-18 05:33:04 -0300

    Várias funções implementadas

--- app/src/main/java/ursal/ursal/LoginActivity.java
+++ app/src/main/java/ursal/ursal/LoginActivity.java
@@ -1,26 +1,32 @@
 package ursal.ursal;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    Button questionario = (Button) findViewById(R.id.btn_questionario);
-    Button fascista = (Button) findViewById(R.id.btn_fascista);
+    Button questionario;
+    Button fascista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_login);
+
+        questionario = findViewById(R.id.btn_questionario);
+        fascista = findViewById(R.id.btn_fascista);
+
 
         questionario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent i = new Intent(getBaseContext(), QuestionarioActivity.class);
+                startActivity(i);
             }
         });
 
@@ -38,5 +44,10 @@ public class Main2Activity extends AppCompatActivity {
         int duration = Toast.LENGTH_LONG;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.moveTaskToBack(true);
     }
 }
---
+++ app/src/main/java/ursal/ursal/QuestionarioActivity.java
@@ -0,0 +1,13 @@
+package ursal.ursal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionario);
+    }
+}
