42fb693cb7ba8720b2f59ddc4b423e18251ee728
Bruno Weber <bruno998>
2018-08-18 03:05:37 -0300

    Hino Sovieti colocado

--- app/src/main/java/ursal/ursal/MainActivity.java
+++ app/src/main/java/ursal/ursal/MainActivity.java
@@ -1,13 +1,28 @@
 package ursal.ursal;
 
+import android.app.Activity;
+import android.app.RemoteAction;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    MediaPlayer ursalMP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ursalMP = MediaPlayer.create(this, R.raw.hino);
+        ursalMP.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ursalMP.stop();
+        ursalMP.release();
+
     }
 }
