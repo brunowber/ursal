1f11024fc171adaa9d38b0b89317d22dd260bdd7
Bruno Weber <bruno998>
2018-08-18 04:04:46 -0300

    Ir para segunda tela

--- app/src/main/java/ursal/ursal/MainActivity.java
+++ app/src/main/java/ursal/ursal/MainActivity.java
@@ -2,13 +2,17 @@ package ursal.ursal;
 
 import android.app.Activity;
 import android.app.RemoteAction;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     MediaPlayer ursalMP;
+    Button btnUrsal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,15 @@ public class MainActivity extends Activity {
 
         ursalMP = MediaPlayer.create(this, R.raw.hino);
         ursalMP.start();
+
+        btnUrsal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), Main2Activity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
     @Override
