4944cd4346007d89a68796a28e6ce6aa362de685
brunowber <brunowber@gmail.com>
2018-08-20 23:23:26 -0300

    Mudanças no Layout

--- app/src/main/java/ursal/ursal/MainActivity.java
+++ app/src/main/java/ursal/ursal/MainActivity.java
@@ -1,18 +1,22 @@
 package ursal.ursal;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     DataBaseHelper db;
     MediaPlayer ursalMP;
     Button btnUrsal;
+    Button buttonFascista;
+    Button buttonFascista2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,9 @@ public class MainActivity extends Activity {
         ursalMP.start();
 
         btnUrsal = findViewById(R.id.button_ursal);
+        buttonFascista = findViewById(R.id.button3);
+        buttonFascista2 = findViewById(R.id.button2);
+
         btnUrsal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,14 +38,27 @@ public class MainActivity extends Activity {
                 startActivity(i);
             }
         });
+        buttonFascista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast("FASCISTA!");
+            }
+        });
+        buttonFascista2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast("FASCISTA!");
+            }
+        });
 
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        ursalMP.stop();
-        ursalMP.release();
-
+    public void toast(String msg){
+        Context context = getApplicationContext();
+        CharSequence text = msg;
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
+
 }
